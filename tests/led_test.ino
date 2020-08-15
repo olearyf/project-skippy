dc4dd8ad22e7acf5eb01df0977bf383b9d1ba9c6
Frances O'Leary <fraoleary@gmail.com>
2020-08-15 16:05:44 -0400

    adding LED test

---
+++ tests/led_test.ino
@@ -0,0 +1,18 @@
+/*
+ * Frances O'Leary, 8/15/2020
+ * 
+ * This is a sanity check for when you've soldered
+ * everything together and want to make sure the
+ * basic connections work ;).
+ * 
+ */
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+
+}
+
+void loop() {
+
+}
